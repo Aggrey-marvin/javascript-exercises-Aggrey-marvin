f73f6a078ee947c069e6ff5ddeaea69bd440a386
Aggrey-marvin <zijjaaggreymarvin@gmail.com>
2024-07-17 05:15:54 +0300

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, number) {
+    if (number < 0) {
+        return "ERROR";
+    }
+    let finalStr = "";
+    for(let i = 0; i < number; i++) {
+        finalStr += str;
+    }
+    return finalStr;
 };
 
 // Do not edit below this line
